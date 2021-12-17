2bcd373dfa71a4bb5b113fc35648e0a4d2eddbfb
Jeongho Park <56580445+JPumpkin645@users.noreply.github.com>
2021-12-18 03:56:40 +0900

    Day 5
    Using classlist instead classname

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
 const h1 = document.querySelector(".hello h1");
-
 function handleTitleClick() {
-  h1.style.color = "blue";
+  const clickedClass = "active";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
+  } else {
+    h1.classList.add(clickedClass);
+  }
 }
-function handleMouseEnter() {
-  h1.innerText = "mouse is here!";
-}
-function handleMouseLeave() {
-  h1.innerText = "mouse is gone!";
-}
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy() {
-  alert("No!");
-}
-function handleWindowOffline() {
-  alert("SOS no Wifi");
-}
-function handleWindowOnline() {
-  alert("Good!");
-}
-h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
 
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
