86d2c596cc6e2420d1d81d5563635e1824bbbab4
Jeongho Park <56580445+JPumpkin645@users.noreply.github.com>
2021-12-18 02:49:44 +0900

    Day5
    addEventListner

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-const daysOfweek = ["mon", "tue", "wed", "thu", "fri", "sat"];
-console.log(daysOfweek);
-daysOfweek.push("sun");
-console.log(daysOfweek);
+const title = document.querySelector(".hello h1");
+
+function handleTitleClick() {
+  title.style.color = "blue";
+}
+function handleMouseEnter() {
+  title.innerText = "mouse is here!";
+}
+function handleMouseLeave() {
+  title.innerText = "mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
