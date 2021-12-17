0c8f620496f20a50adc4bc0b64db6fe08c265231
Jeongho Park <56580445+JPumpkin645@users.noreply.github.com>
2021-12-18 03:10:33 +0900

    Day 5
    add events about window size and wifi

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  title.style.color = "blue";
+  h1.style.color = "blue";
 }
 function handleMouseEnter() {
-  title.innerText = "mouse is here!";
+  h1.innerText = "mouse is here!";
 }
 function handleMouseLeave() {
-  title.innerText = "mouse is gone!";
+  h1.innerText = "mouse is gone!";
 }
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+function handleWindowCopy() {
+  alert("No!");
+}
+function handleWindowOffline() {
+  alert("SOS no Wifi");
+}
+function handleWindowOnline() {
+  alert("Good!");
+}
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
