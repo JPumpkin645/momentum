b69eca838bfa94343b673e7791c7ef200c2eccd2
Jeongho Park <56580445+JPumpkin645@users.noreply.github.com>
2022-01-17 16:12:00 +0900

    reset

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-const body = document.body;
 
-function WindowHanddleResize() {
-  var wWidth = window.innerWidth;
-  if (wWidth <= 400) {
-    body.className = "";
-  } else if (wWidth > 400 && wWidth <= 700) {
-    body.className = "middle";
-  } else if (wWidth > 700) {
-    body.className = "large";
-  }
-}
-window.addEventListener("resize", WindowHanddleResize);
-//400//700//이상
