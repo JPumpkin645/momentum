b453605c50ca447029fb34f70d47828895ba6085
Jeongho Park <56580445+JPumpkin645@users.noreply.github.com>
2022-02-12 14:44:11 +0900

    add background images in page

---
+++ js/background.js
@@ -0,0 +1,5 @@
+const randomImgNumber = Math.floor(Math.random() * 19) + 1;
+const randomImg = `${randomImgNumber}` + ".jpg";
+const bgImage = document.createElement("img");
+bgImage.src = `img/${randomImg}`;
+document.body.appendChild(bgImage);
